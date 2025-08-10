660dc9dceee1b50f9c5c31c4c9f4220de0113a89
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2025-08-10 19:53:50 -0400

    Add getBarrelFileReferencesInFile utility

---
+++ src/getBarrelFileReferencesInFile.test.ts
@@ -0,0 +1,51 @@
+import { afterEach, describe, it } from 'node:test';
+import assert from 'node:assert/strict';
+import mock from 'mock-fs';
+
+import { getBarrelFileReferencesInFile } from './getBarrelFileReferencesInFile';
+
+describe('getBarrelFileReferencesInFile tests', () => {
+  afterEach(() => {
+    mock.restore();
+  });
+
+  it('returns no results when the module has no exports', () => {
+    mock({
+      '/index.ts': 'export {} from "./test";',
+      '/test.ts': 'export const test = 1;',
+      './node_modules': mock.load('node_modules'),
+    });
+
+    assert.deepEqual(getBarrelFileReferencesInFile('/index.ts'), []);
+  });
+
+  it('returns no results when the module does not reference other barrel files', () => {
+    mock({
+      '/index.ts': 'export { test } from "./test";',
+      '/test.ts': 'export const test = 1;',
+      './node_modules': mock.load('node_modules'),
+    });
+
+    assert.deepEqual(getBarrelFileReferencesInFile('/index.ts'), []);
+  });
+
+  it('returns references to other barrel files', () => {
+    mock({
+      '/index.ts': `
+      export { test } from "./test";
+      export { barrelFileReference } from "./barrelFileReference";
+      export { reExportFromExternalPackage } from "react";
+      `,
+      '/test.ts': 'export const test = 1;',
+      '/barrelFileReference/index.ts': 'export { barrelFileReference } from "./barrelFileReference";',
+      '/barrelFileReference/barrelFileReference.ts': 'export const barrelFileReference = 12',
+
+      './node_modules': mock.load('node_modules'),
+    });
+
+    assert.deepEqual(getBarrelFileReferencesInFile('/index.ts'), [
+      { barrelFilePath: '/barrelFileIndex/index.ts' },
+      { barrelFilePath: 'react' },
+    ]);
+  });
+});
---
+++ src/getBarrelFileReferencesInFile.ts
@@ -0,0 +1,44 @@
+import { getExportsFromModule } from './getExportsFromModule';
+import path from 'node:path';
+import { resolveModulePath } from './resolveModulePath';
+import { convertAbsolutePathToRelativeImportPath, isInternalModule } from './importUtils';
+
+export interface BarrelFileReference {
+  /** The relative path to the barrel file that was found */
+  barrelFilePath: string;
+}
+
+/**
+ * Analyzes the exports of the provided file and returns any references to barrel files.
+ * @param absoluteFilePath The absolute path the the file we will analyze
+ * @returns An array of barrel file references found in the file
+ */
+export function getBarrelFileReferencesInFile(absoluteFilePath: string): BarrelFileReference[] {
+  const result: BarrelFileReference[] = [];
+
+  const exports = getExportsFromModule(absoluteFilePath);
+
+  for (const reExportToVisit of exports.reExports) {
+    if (isInternalModule(reExportToVisit.importPath)) {
+      const potentialBarrelFilePath = resolveModulePath(
+        path.resolve(path.dirname(absoluteFilePath), reExportToVisit.importPath)
+      );
+      const exportsFromPotentialBarrel = getExportsFromModule(potentialBarrelFilePath);
+
+      if (exportsFromPotentialBarrel.reExports.length > 0) {
+        result.push({
+          barrelFilePath: convertAbsolutePathToRelativeImportPath(
+            potentialBarrelFilePath,
+            path.dirname(absoluteFilePath)
+          ),
+        });
+      }
+    }
+
+    result.push({
+      barrelFilePath: reExportToVisit.importPath,
+    });
+  }
+
+  return result;
+}
