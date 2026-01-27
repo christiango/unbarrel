31177297522a72f45f5dbb8f1ce61a20bec07eae
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2026-01-27 10:09:43 -0500

    More work in progress

--- src/fixIssuesInBarrelFile.ts
+++ src/fixIssuesInBarrelFile.ts
@@ -4,7 +4,7 @@ import traverse from '@babel/traverse';
 import generate from '@babel/generator';
 import * as t from '@babel/types';
 import { flattenExportStar } from './flattenExportStar';
-import { getExportDefinitionFromReExport, ResolvedModuleDefinition } from './getExportDefinitionFromReExport';
+import { getExportDefinitionFromReExport } from './getExportDefinitionFromReExport';
 import { parseTypescriptFile } from './parseUtils';
 import { isBarrelFileReference } from './getIssuesInBarrelFile';
 import { isInternalModule } from './importUtils';
@@ -22,6 +22,33 @@ interface ExportNamedDeclarationStatement {
 }
 
 type ExportStatement = ExportStartStatement | ExportNamedDeclarationStatement;
+
+/** After all the processing has been done */
+function cleanupExportsInAST(ast: babel.types.File) {
+  // Map to the first export statement with a given path
+  const pathToExportMap = new Map<string, babel.NodePath<t.ExportNamedDeclaration>>();
+
+  traverse(ast, {
+    ExportNamedDeclaration(path) {
+      if (path.node.source) {
+        if (path.node.specifiers.length === 0) {
+          // If there are no specifiers left, remove the export statement
+          path.remove();
+          return;
+        }
+
+        const existingExportForPath = pathToExportMap.get(path.node.source.value);
+        if (existingExportForPath) {
+          // If we have seen the same path already, merge the specifiers and remove the current one
+          existingExportForPath.node.specifiers.push(...path.node.specifiers);
+          path.remove();
+        } else {
+          pathToExportMap.set(path.node.source.value, path);
+        }
+      }
+    },
+  });
+}
 
 /**
  * Fixes any issues in the specified barrel file.
@@ -85,10 +112,40 @@ export function fixIssuesInBarrelFile(absoluteFilePath: string) {
           const exportedName = specifier.exported.name;
           const importedName = specifier.local.name;
 
+          // If this export points to another barrel file, let's replace it with a direct reference to the true source module
           if (
             isInternalModule(exportStatement.sourcePath) &&
             isBarrelFileReference(absoluteFilePath, exportStatement.sourcePath, importedName)
           ) {
+            const typeOnly = specifier.exportKind === 'type' || exportStatement.nodePath.node.exportKind === 'type';
+            const resolvedExport = getExportDefinitionFromReExport(absoluteFilePath, {
+              type: importedName === 'default' ? 'defaultExport' : 'namedExport',
+              importedName,
+              exportedName,
+              importPath: exportStatement.sourcePath,
+              typeOnly,
+            });
+
+            // Add a new export statement to the true path
+            exportStatement.nodePath.insertAfter(
+              t.exportNamedDeclaration(
+                null,
+                [
+                  t.exportSpecifier(
+                    t.identifier(resolvedExport.importedName),
+                    t.identifier(resolvedExport.exportedName)
+                  ),
+                ],
+                t.stringLiteral(resolvedExport.importPath)
+              )
+            );
+
+            // Remove the specifier from the current export statement
+            exportStatement.nodePath.node.specifiers = exportStatement.nodePath.node.specifiers.filter(
+              (s) => s !== specifier
+            );
+
+            astModified = true;
           }
         }
       }
@@ -97,6 +154,7 @@ export function fixIssuesInBarrelFile(absoluteFilePath: string) {
 
   // Write output if any changes were made
   if (astModified) {
+    cleanupExportsInAST(ast);
     const output = generate(ast, { retainLines: false, comments: true });
     fs.writeFileSync(absoluteFilePath, output.code, 'utf8');
   }
